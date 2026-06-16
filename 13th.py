4a347b0b16db6cc980011c6bafbe22186ba8acfb
kumaranna04130-debug <www.vivekrathor220@gmail.com>
2026-06-16 12:27:45 +0530

    2d array in numpy
    This repository demonstrates how to create and print a 2D array using NumPy.

---
+++ 13th.py
@@ -0,0 +1,11 @@
+#import numpy as np
+
+#marks = np.array([85,83,83])
+#print("marks:",marks)
+
+import numpy as np
+
+arr_2d = np.array([85,83,82],
+               [70,60,50],
+               [90,70,80])
+print(arr_2d)
